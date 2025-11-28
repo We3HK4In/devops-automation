9eb816853fee282df62a26f015aa73a95a619fec
We3HK4In <95773551+We3HK4In@users.noreply.github.com>
2025-11-28 13:19:41 -0500

    Increase loop iterations from 5 to 6

--- Anis.py
+++ Anis.py
@@ -6,8 +6,9 @@ print("--- Python Application Started ---")
 print("Running the application script.")
 
 # For simplicity, we'll just print status updates.
+# Will slightly modify the app, so that docker builds a new image and changes its tag from latest to Recording.
 
-for i in range(5):
+for i in range(6):
     print(f"Application Status: Running... (Cycle {i+1})")
     time.sleep(1)
 
